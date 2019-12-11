f3f3dd19ae21e1fd8026a3c53c73e5c8cd40f2c6
Leisha <banksleisha@yahoo.com>
2019-12-10 23:32:08 -0500

    initial commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const App = () => {
+  const [newMember, setNewMember] = useState( [] );
+
+    const addNewMember = 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
